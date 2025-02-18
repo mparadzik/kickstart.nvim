1e0c086abfee83a4a362090dc2a777fa46385ab5
matej.paradzik <matej.paradzik@gmail.com>
2025-02-18 10:36:36 +0100

    oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+	  'stevearc/oil.nvim',
+	    config = function()
+	      local oil = require 'oil'
+	      oil.setup()
+	      -- Keymap for opening parent directory in oil.nvim
+	      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+	    end,
+	  ---@module 'oil'
+	  ---@type oil.SetupOpts
+	  opts = {},
+	  -- Optional dependencies
+	  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	  lazy = false,
+	  
+	},
+}
